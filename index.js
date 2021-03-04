656d3ba51cb4ddbfa729261b9215854ea0265181
Olukayode <olukayodeshokunbi@gmail.com>
2021-03-04 18:44:08 -0500

    Created routes module to handle CRUD operations for card documents

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 const connectDB = require("./starter/database");
+const express = require("express");
+const app = express();
+const cards = require("./routes/cards");
 
 connectDB();
+
+app.use(express.json());
+app.use("api/cards", cards);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server Started on PORT ${PORT}`);
+});
