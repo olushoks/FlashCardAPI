38b3329c4e72e493aab72b20fb356b2983449520
Olukayode <olukayodeshokunbi@gmail.com>
2021-03-04 20:59:50 -0500

    Built validation function that checks the accuracy of user data to ensure it matched the Schema before it is sent over

--- models/card.js
+++ models/card.js
@@ -1,14 +1,24 @@
 const mongoose = require("mongoose");
-
+const Joi = require("joi");
 // CARD SCHEMA
 const cardSchema = new mongoose.Schema({
   question: { type: String, required: true, minlength: 1 },
   answer: { type: String, required: true, minlength: 1 },
-  dateCreated: { type: Date, default: Date.now },
-  lastTimeModfied: { type: Date, default: Date.now },
+  lastDateModfied: { type: Date, default: Date.now },
 });
 
 // CARD MODEL
 const Card = mongoose.model("Card", cardSchema);
 
-module.exports = Card;
+function validateCard(card) {
+  const schema = Joi.object({
+    question: Joi.string().required().minlength(1),
+    answer: Joi.String().required().minlength(1),
+    lastDateModfied: Joi.Date().default(Date.now),
+  });
+  return schema.validate(card);
+}
+
+exports.cardSchema = cardSchema;
+exports.Card = Card;
+exports.validate = validateCard;
